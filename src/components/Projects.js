c5e44c185ca7e9e67cc55cedfe660a989a4dbd55
Drishti Saraf <drishtisaraf0307@gmail.com>
2024-07-06 21:41:28 +0530

    projects added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-import projImg1 from "../assessts/images/project-img1.png";
-import projImg2 from "../assessts/images/project-img2.png";
-import projImg3 from "../assessts/images/project-img3.png";
+import ecommerce from "../assessts/images/e-commerce.png";
+import crypto from "../assessts/images/crypto.png";
+import modernui from "../assessts/images/modernReact.png";
+import apple from "../assessts/images/apple.png";
+import botcamp from "../assessts/images/bootcamp.png";
+import travel from "../assessts/images/spaceTravel.png"
 import colorSharp2 from "../assessts/images/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -12,34 +15,34 @@ const Projects = () => {
 
     const projects = [
         {
-          title: "Business Startup",
-          description: "Design & Development",
-          imgUrl: projImg1,
+          title: "E-commerce Website",
+          imgUrl: ecommerce,
+          link:"https://github.com/drishti1920/ecommerce-candle"
         },
         {
-          title: "Business Startup",
-          description: "Design & Development",
-          imgUrl: projImg2,
+          title: "Crypto Tracker",
+          imgUrl: crypto,
+          link:"https://github.com/drishti1920/crypto-tracker"
         },
         {
-          title: "Business Startup",
-          description: "Design & Development",
-          imgUrl: projImg3,
+          title: "Modern React UI",
+          imgUrl: modernui,
+          link: "https://github.com/drishti1920/modern-react-ui?tab=readme-ov-file"
         },
         {
-          title: "Business Startup",
-          description: "Design & Development",
-          imgUrl: projImg1,
+          title: "Apple Website UI Clone",
+          imgUrl: apple,
+          link:"https://github.com/drishti1920/apple.github.io"
         },
         {
-          title: "Business Startup",
-          description: "Design & Development",
-          imgUrl: projImg2,
+          title: "Space Travel",
+          imgUrl: travel,
+          link:"https://github.com/drishti1920/space-travel"
         },
         {
-          title: "Business Startup",
-          description: "Design & Development",
-          imgUrl: projImg3,
+          title: "Bootcamp Website Design",
+          imgUrl: botcamp,
+          link:"https://github.com/drishti1920/Bootcamp-Website-Design"
         },
       ];
 
@@ -56,15 +59,15 @@ const Projects = () => {
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                    <Nav.Item>
+                    {/* <Nav.Item>
                       <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
+                    </Nav.Item> */}
+                    {/* <Nav.Item>
                       <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
+                    </Nav.Item> */}
+                    {/* <Nav.Item>
                       <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                    </Nav.Item>
+                    </Nav.Item> */}
                   </Nav>
                   <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
                     <Tab.Pane eventKey="first">
@@ -84,9 +87,9 @@ const Projects = () => {
                     <Tab.Pane eventKey="second">
                       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
                     </Tab.Pane>
-                    <Tab.Pane eventKey="third">
+                    {/* <Tab.Pane eventKey="third">
                       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
+                    </Tab.Pane> */}
                   </Tab.Content>
                 </Tab.Container>
               </div>}
